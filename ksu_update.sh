85782654bab38d039020ef101db6dbf40c807838
soca indra <ssocaindra2@gmail.com>
2023-12-15 07:08:52 +0000

    Revert "ksu_update.sh: Commit message for squash commit"
    This reverts commit b088f65345bee1e1a162f8c73491309b493980c6.

--- ksu_update.sh
+++ ksu_update.sh
@@ -2,6 +2,6 @@
 
 echo pulling main branch of KernelSU
 
-git subtree pull --prefix=drivers/staging/kernelsu https://github.com/tiann/KernelSU.git main --squash -m "drivers: kernelsu: update"
+git subtree pull --prefix=drivers/staging/kernelsu https://github.com/tiann/KernelSU.git main --squash
 
 echo done
